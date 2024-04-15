9a2ce20f36cc848a615aaa4c09c035f309dc5129
pigrov <pigrov@gmail.com>
2024-04-15 18:58:00 +0300

    Intl time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,23 +4,25 @@ import MeetingTypeList from "@/components/MeetingTypeList";
 import { DateTime } from "luxon";
 
 const Home = () => {
-    const time = new Date().toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-    });
-    const date = new Date().toLocaleDateString("en-US", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-    });
+    // const time = new Date().toLocaleTimeString("en-US", {
+    //     hour: "2-digit",
+    //     minute: "2-digit",
+    //     hour12: true,
+    // });
+    // const date = new Date().toLocaleDateString("en-US", {
+    //     weekday: "long",
+    //     year: "numeric",
+    //     month: "long",
+    //     day: "numeric",
+    // });
 
-    const localTime = DateTime.fromISO(new Date().toLocaleTimeString(), { zone: "system" })
-        .setZone("America/New_York")
-        .toFormat("h:mm a");
+    // const time = DateTime.fromISO(new Date().toLocaleTimeString(), { zone: "system" })
+    //     .setZone("America/New_York")
+    //     .toFormat("h:mm a");
 
-    console.log(localTime);
+    const now = new Date();
+    const time = now.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
+    const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
     return (
         <section className="flex size-full flex-col gap-10 text-white">
@@ -30,7 +32,7 @@ const Home = () => {
                         Upcoming Meeting at 13:45
                     </h2>
                     <div className="flex flex-col gap-2">
-                        <h1 className="text-3xl lg:text-7xl font-extrabold">{localTime}</h1>
+                        <h1 className="text-3xl lg:text-7xl font-extrabold">{time}</h1>
                         <p className="text-lg font-medium text-slate-400">{date}</p>
                     </div>
                 </div>
